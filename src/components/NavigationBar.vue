cda29f610ee84554a419336e8108e0e0b15e8572
usernameiskheejay <galvez.kjm@optimixr.com>
2024-11-14 08:40:18 +0800

    generic commit

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="w-full max-w-[100rem] mx-auto flex justify-between items-center xl:px-12 sticky top-0 bg-white shadow">
-        <div class="w-[9rem] h-[4rem] flex justify-center items-center">
+    <div class="w-full max-w-[100rem] mx-auto flex justify-between items-center px-2 xl:px-12 sticky top-0 bg-white shadow">
+        <div class="sm:w-[9rem] h-[3.5rem] sm:h-[4rem] flex justify-center items-center">
             <IPSYNCLogo class="w-[5rem]" />
         </div>
-        <div class="hidden md:flex gap-[1.75rem] lg:gap-[4rem] text-nowrap">
+        <div class="hidden md:flex gap-[2rem] lg:gap-[4rem] text-nowrap">
             <RouterLink :to="{ name: 'Landing' }"
-                :class="`text-[0.8rem] lg:text-[1rem] text-font ${ $route.name === 'Landing' ? 'font-bold underline' : '' } 
+                :class="`text-[0.8rem] xl:text-[1rem] text-font ${ $route.name === 'Landing' ? 'font-bold underline' : '' } 
                     hover:font-bold hover:underline text-shadow`">
                 HOME
             </RouterLink>
@@ -15,29 +15,29 @@
                 DASHBOARD
             </RouterLink>
             <RouterLink :to="{ name: '' }"
-                :class="`text-[0.8rem] lg:text-[1rem] text-font ${ false ? 'font-bold  underline' : '' } 
+                :class="`text-[0.8rem] xl:text-[1rem] text-font ${ false ? 'font-bold  underline' : '' } 
                     hover:font-bold hover:underline text-shadow`">
                 INTERNSHIPS AND PROJECTS
             </RouterLink>
             <RouterLink :to="{ name: '' }"
-                :class="`text-[0.8rem] lg:text-[1rem] text-font ${ false ? 'font-bold  underline' : '' } 
+                :class="`text-[0.8rem] xl:text-[1rem] text-font ${ false ? 'font-bold  underline' : '' } 
                     hover:font-bold hover:underline text-shadow`">
                 ABOUT US
             </RouterLink>
             <RouterLink :to="{ name: '' }"
-                :class="`text-[0.8rem] lg:text-[1rem] text-font ${ false ? 'font-bold  underline' : '' } 
+                :class="`text-[0.8rem] xl:text-[1rem] text-font ${ false ? 'font-bold  underline' : '' } 
                     hover:font-bold hover:underline text-shadow`">
                 CONTACT US
             </RouterLink>
         </div>
-        <div class="w-[9rem] flex justify-center items-center relative">
+        <div class="sm:w-[9rem] flex justify-center items-center relative">
             <div @mouseup="showDropDown = !showDropDown" class="relative">
                 <img src="../assets/images/jacquard.png" alt="profile" 
                     class="bg-black w-9 h-9 rounded-full border-2 border-c1 cursor-pointer">
                 <ArrowDown  class="absolute top-1/2 -right-1"/> 
             </div>
             <div  v-if="showDropDown" ref="target"
-                class="absolute -bottom-[10.3rem] -left-[8.2rem] h-[9rem] w-[14rem] bg-white rounded-[0.625rem] p-2">
+                class="absolute -bottom-[10.3rem] -left-[11.66rem] sm:-left-[8.2rem] h-[9rem] w-[14rem] bg-white rounded-[0.625rem] p-2">
                 <div class="flex p-2">
                     <img src="../assets/images/jacquard.png" alt="" 
                         class="bg-black w-11 h-11 rounded-full border-2 border-c1">
