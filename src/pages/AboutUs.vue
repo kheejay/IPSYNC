c9979c58249912d01455c6ffe715011ae49a71e1
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2024-11-15 02:38:15 +0800

    Create AboutUs.vue

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,161 @@
+<template>
+    <div>
+        <div class="bg-image text-white text-center">
+            <!--About us here-->
+            <div class="font-extrabold text-[3rem] pt-[6rem] md:pt-[4rem] ">
+                ABOUT US
+            </div>
+            <div class="tracking-widest font-light px-[3rem] text-[0.85rem] md:text-[1rem] sm:text-[0.9rem]">
+                IPSync is a platform that connects students, faculty mentors, and industry partners from West Visayas State University to foster hands-on learning and career readiness.
+            </div>
+        </div>
+        <!--Our Mission naman here-->
+        <div class="h-auto bg-c1 text-white mt-12 leading-[1.75rem]">
+            <div class="font-extrabold text-[3rem] pt-[10rem] md:pt-[8rem] text-center pb-4 mb-6">
+                OUR MISSION 
+            </div>
+            <div class="px-[5%] xl:px-[10%] ">
+                <div class="md:flex md:grid-flow-row lg:grid-cols-2 gap-5">
+                    <!--Mission 1-->
+                    <div class="xl:w-[45%] md:w-[50%] flex md:justify-end justify-center">
+                        <img src="/src/assets/images/aboutBook.png" alt="Mission 1" class="h-[14rem] w-auto md:h-[16rem]" />
+                    </div>
+                    <div class="xl:w-[50%] lg:w-[60%] md:w-[70%] tracking-widest md:pr-[6rem] md:text-start text-center">
+                        <div class="font-bold md:w-[65%]">
+                            Access to Internships & Projects for Skill Development
+                        </div>
+                        <div class="font-light">
+                            We offer students a wide range of internships and project-based opportunities that allow them to apply their academic knowledge in real-world settings. These experiences help students develop practical skills, gain professional insights, and build a strong portfolio to enhance their career prospects.
+                        </div>
+                    </div>
+                </div>
+                <!--Mission 2-->
+                <div class="md:flex md:grid-flow-row lg:grid-cols-2 gap-5 pt-[4rem]">
+                    <div class="xl:w-[45%] md:w-[50%] flex md:justify-end justify-center">
+                        <img src="/src/assets/images/aboutLaptop.png" alt="Mission 1" class="h-[14rem] w-auto md:h-[16rem]" />
+                    </div>
+                    <div class="xl:w-[50%] lg:w-[60%] md:w-[70%] tracking-widest md:pr-[6rem] md:text-start text-center">
+                        <div class="font-bold md:w-[65%]">
+                            Platform for Faculty & Industry to Connect with Students
+                        </div>
+                        <div class="font-light">
+                            Our platform enables faculty, mentors, and industry partners to easily post internship and project opportunities, allowing them to directly connect with students. This collaboration provides students with valuable real-world experiences and helps industry partners find fresh talent and innovative ideas.
+                        </div>
+                    </div>
+                </div>
+                <!--Mission 3-->
+                <div class="md:flex md:grid-flow-row lg:grid-cols-2 gap-5 pt-[4rem] pb-[6rem]">
+                    <div class="md:w-[40%] flex md:justify-end justify-center">
+                        <img src="/src/assets/images/aboutBulb.png" alt="Mission 1" class="h-[14rem] w-auto md:h-[16rem]" />
+                    </div>
+                    <div class="xl:w-[50%] lg:w-[50%] md:w-[70%] tracking-widest md:pr-[6rem] md:text-start text-center md:pl-[3rem] xl:pl-[4rem]">
+                        <div class="font-bold md:w-[65%]">
+                            Bridging Academia and Industry Through Experiential Learning
+                        </div>
+                        <div class="font-light">
+                            We foster a culture of experiential learning by connecting academic theory with practical application. Through internships, collaborative projects, and mentorships, students gain hands-on experience that deepens their understanding of their field and prepares them to succeed in the workforce.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--Meet the team here-->
+        <div class="bg-c1 h-auto">
+            <div class="font-extrabold mt-12 text-[3rem] text-center text-white pt-20">
+                MEET THE TEAM
+            </div>
+            <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-10 justify-evenly text-center px-[10%] pb-[10rem]">
+                <!--em-->
+                <div class="flex flex-col items-center text-center">
+                    <div>
+                        <img src="/src/assets/images/mark.png" alt="Person 1" class="h-[18rem] w-auto" />
+                    </div>
+                    <div class="font-bold text-white tracking-wider mt-2">
+                        Mark Andrei Encanto
+                    </div>
+                    <div class="text-white tracking-wider">
+                        PROJECT MANAGER
+                    </div>
+                </div>
+                <!--kj-->
+                <div class="flex flex-col items-center text-center">
+                    <div>
+                        <img src="/src/assets/images/kj.png" alt="Person 1" class="h-[18rem] w-auto" />
+                    </div>
+                    <div class="font-bold text-white tracking-wider mt-2">
+                        Khee Jay Galvez
+                    </div>
+                    <div class="text-white tracking-wider">
+                        DEVELOPER
+                    </div>
+                </div>
+                <!--cha-->
+                <div class="flex flex-col items-center text-center">
+                    <div>
+                        <img src="/src/assets/images/chariz.png" alt="Person 1" class="h-[18rem] w-auto" />
+                    </div>
+                    <div class="font-bold text-white tracking-wider mt-2">
+                        Chariz Dianne Falco
+                    </div>
+                    <div class="text-white tracking-wider">
+                        QUALITY ASSURANCE OFFICER
+                    </div>
+                </div>
+                <!--che-->
+                <div class="flex flex-col items-center text-center">
+                    <div>
+                        <img src="/src/assets/images/che.png" alt="Person 1" class="h-[18rem] w-auto" />
+                    </div>
+                    <div class="font-bold text-white tracking-wider mt-2">
+                        Cherilyn Marie Deocampo
+                    </div>
+                    <div class="text-white tracking-wider">
+                        UI/UX
+                    </div>
+                </div>
+                <!--jeph-->
+                <div class="flex flex-col items-center text-center">
+                    <div>
+                        <img src="/src/assets/images/jeph.png" alt="Person 1" class="h-[18rem] w-auto" />
+                    </div>
+                    <div class="font-bold text-white tracking-wider mt-2">
+                        Jephone Israel Jireh Torre
+                    </div>
+                    <div class="text-white tracking-wider">
+                        UI/UX
+                    </div>
+                </div>
+                <!--allan-->
+                <div class="flex flex-col items-center text-center">
+                    <div>
+                        <img src="/src/assets/images/allan.png" alt="Person 1" class="h-[18rem] w-auto" />
+                    </div>
+                    <div class="font-bold text-white tracking-wider mt-2">
+                        Allan Andrews Tacuel
+                    </div>
+                    <div class="text-white tracking-wider">
+                        ILLUSTRATOR/ASSET
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.bg-image {
+    background-image: url('/src/assets/images/aboutBg.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 100vh;
+}
+
+@media (min-width: 48rem) {
+    .bg-image {
+        height: calc(100vh - 4rem); 
+    }
+}
+</style>
