39233ca7dee62351af355e5b8133b48df51f677a
usernameiskheejay <galvez.kjm@optimixr.com>
2024-11-15 08:10:44 +0800

    head profile non reponsive

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="z-[1] w-full max-w-[100rem] mx-auto flex justify-between items-center px-3 xl:px-12 sticky top-0 bg-white shadow">
+    <div class="z-[3] w-full max-w-[100rem] mx-auto flex justify-between items-center px-3 xl:px-12 sticky top-0 bg-white shadow">
         <div class="md:w-[9rem] h-[4rem] flex justify-center items-center">
             <IPSYNCLogo class="w-[3rem] md:w-[5rem]" />
         </div>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,32 +1,71 @@
 <template>
-    <div>
-        <div class="p-4 text-xl border">{{ 'Reflected value from store: ' + a }}</div>
-        <div @click="$router.push({ name: 'Dashboard' })" class="p-4 border w-max">Dashboard</div>
-        <div @click="handleLogout" class="p-4 w-max border cursor-pointer">Logout</div>
-        <p class="p-4 bg-sky-400">{{ user.displayName }}</p>
+<div class="w-full flex justify-center">
+    <div class="w-full xl:w-[60rem] py-9">
+        
+        <div class="w-full min-h-[53rem] h-[53rem] flex flex-col shadow-[0rem_0.25rem_0.25rem_black] 
+            rounded-[0.625rem] relative overflow-hidden">
+            <div class="xl:w-[55rem] xl:h-[55rem] bg-c1 absolute rounded-full -top-[15rem] -left-[15.5rem]"></div>
+            <div class="xl:w-[30rem] xl:h-[30rem] bg-[#4A658D] absolute top-0 rounded-lg -right-[15rem]"></div>
+            <div class="z-[2] absolute w-[12rem] h-[12rem] bg-white rounded-full left-[4rem] top-[13rem] flex items-center 
+            justify-center border border-c1 hover:cursor-pointer overflow-hidden">
+                <div class="w-full h-full border-[0.2rem] rounded-full border-white z-[2] absolute"></div>
+                <img src="/src/assets/images/jacquard.png" alt="user photo" class="bg-black">
+            </div>
+            <div class="w-full h-[40%] bg-transparent bg-black"></div>
+            <div class="z-[1] w-full h-[60%] bg-white rounded-[0.625rem] ring-1 ring-inset ring-black p-4">
+                <div class="w-full flex justify-end items-center gap-4">
+                    <button class="rounded-full bg-c1 text-white py-[0.2rem] md:py-[0.4rem]
+                        my-3 w-[10rem] text-[0.90rem] sm:text-[1rem] active:scale-[99%]">
+                        Save Changes
+                    </button>
+                    <EditProfileInfo class="w-[2rem] h-[2rem] cursor-pointer 
+                        hover:scale-105 active:scale-95 duration-200" />
+                </div>
+                <div class="w-full flex flex-col items-start">
+                    <div class="w-full ring-inset focus-within:ring-1 focus-within:ring-c1">
+                        <input type="text" class="w-full h-full bg-transparent px-2 text-[1.80rem] 
+                            focus:outline-none" value="Chicken Bilog Doe">
+                    </div>
+                    <div class="w-full ring-inset focus-within:ring-1 focus-within:ring-c1">
+                        <input type="text" class="w-full bg-transparent p-2 text-[1.1rem] italic text-c1
+                            focus:outline-none" 
+                            value="College of Senior High School">
+                    </div>
+                </div>
+                <div class="w-full font-bold text-c1 text-[1.75rem] mt-[3.5rem]">About Me</div>
+                <div class="w-full h-[9rem] flex items-center mt-[2rem]">
+                    <div class="w-[40%] grid gap-4 py-2 border-r-2 border-black pr-4">
+                        <div class="w-full ring-inset focus-within:ring-1 focus-within:ring-c1">
+                            <input type="text" class="w-full bg-transparent text-[1.1rem] placeholder:italic p-2 text-c1 focus:outline-none" 
+                            placeholder="Degree Program">
+                        </div>
+                        <div class="w-full ring-inset focus-within:ring-1 focus-within:ring-c1">
+                            <input type="text" class="w-full bg-transparent text-[1.1rem] placeholder:italic p-2 text-c1 focus:outline-none" 
+                            placeholder="Year Level">
+                        </div>
+                        <div class="w-full ring-inset focus-within:ring-1 focus-within:ring-c1">
+                            <input type="text" class="w-full bg-transparent text-[1.1rem] placeholder:italic p-2 text-c1 focus:outline-none" 
+                            placeholder="Student ID">
+                        </div>
+                    </div>
+                    <div class="w-[60%] h-full px-4">
+                        <textarea class="w-full min-h-[9rem] border p-2 focus:outline-none focus:border focus:border-c1
+                                resize-none max-h-[9rem]"
+                                ref="textarea"
+                                v-model="input"
+                                placeholder="What's on your mind?"
+                            />
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
+</div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useCounterMyVersion } from '../store/tests/counter';
-import { userState } from '../store/authState'
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase';
-import { useRouter } from 'vue-router';
-
-const user = userState();
-
-const counter = useCounterMyVersion();
-
-const a = ref(counter.count)
-const router = useRouter()
-const handleLogout = () => {
-    signOut(auth).then(() => {
-        router.push({ name: 'Login' })
-    }).catch((error) => {
-
-    })
-}
-
+import EditProfileInfo from '../components/icons/EditProfileInfo.vue'
+import { useTextareaAutosize } from '@vueuse/core';
+const { textarea, input } = useTextareaAutosize()
 </script>
