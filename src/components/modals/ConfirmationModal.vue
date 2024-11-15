0c8154da214fddd279e35e7f127633343e8cd3c7
usernameiskheejay <galvez.kjm@optimixr.com>
2024-11-15 10:35:28 +0800

    handle opacity

--- src/components/modals/ConfirmationModal.vue
+++ src/components/modals/ConfirmationModal.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="z-[2] fixed w-screen h-screen top-0 left-0 bg-c2 opacity-50"></div>
-<div class="z-[2] flex flex-col gap-4 sm:gap-6 p-6 bg-c4 shadow-[0rem_0.25rem_0.25rem_black] border border-black w-[90%] sm:w-max
+<div class="z-[3] fixed w-screen h-screen top-0 left-0 bg-c2 opacity-50"></div>
+<div class="z-[3] flex flex-col gap-4 sm:gap-6 p-6 bg-c4 shadow-[0rem_0.25rem_0.25rem_black] border border-black w-[90%] sm:w-max
     fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
     <div class="w-full sm:w-[20rem]">{{ message ?? 'Please confirm to do this action.' }}</div>
     <div class="w-full flex items-center gap-4">
