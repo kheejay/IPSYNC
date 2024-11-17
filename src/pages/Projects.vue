eef07d73aaf1199312ad839942b5d814398483fd
usernameiskheejay <galvez.kjm@optimixr.com>
2024-11-17 16:13:23 +0800

    commit message

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -7,10 +7,10 @@
                 <img src="/src/assets/images/WestPeople.svg" alt="Group Photo" class="w-[30rem] md:w-1/2 h-auto">
                 <div class="flex-grow flex items-center pb-[3rem]">
                     <div class="w-full flex flex-col items-center xl:max-w-[42rem]">
-                        <div class="w-full text-[1.50rem] xl:text-[2rem] font-extrabold text-c1">
+                        <div class="w-full text-[1.75rem] xl:text-[2rem] font-extrabold text-c1">
                             TAGA-WEST
                         </div>
-                        <div class="w-full text-[1.65rem] xl:text-[2.50rem] font-semibold text-c6 mt-1.5 xl:mt-6 leading-[2.25rem]">
+                        <div class="w-full text-[2rem] xl:text-[2.50rem] font-semibold text-c6 mt-1.5 xl:mt-6 leading-[2.25rem]">
                             Looking to post a project?
                         </div>
                         <div class="text-[1rem] sm:text-[1.25rem] xl:text-[1.50rem] text-c6 xl:text-start mt-4">
@@ -27,13 +27,13 @@
                 </div>
             </div>
 
-            <div class="w-full flex flex-col-reverse md:flex-row">
-                <div class="flex-grow flex items-center pb-[3rem] translate-x-[4rem]">
+            <div class="w-full flex flex-col md:flex-row">
+                <div class="flex-grow flex items-center pb-[3rem] md:translate-x-[4rem] mt-12">
                     <div class="w-full flex flex-col items-center xl:max-w-[42rem]">
-                        <div class="w-full text-[1.50rem] xl:text-[4rem] font-bold text-c1">
+                        <div class="w-full text-[3rem] xl:text-[4rem] font-bold text-c1 leading-[3rem] xl:leading-[4rem]">
                             <span class="text-c6">Find the</span> Perfect Job
                         </div>
-                        <div class="text-[1rem] sm:text-[1.35rem] xl:text-[1.50rem] text-c1 xl:text-start pr-[5rem]">
+                        <div class="text-[1rem] sm:text-[1.35rem] xl:text-[1.50rem] text-c1 xl:text-start pr-[5rem] mt-[0.8rem]">
                             Discover internships and projects seeking talent like you—each is a chance to grow, build skills, and gain hands-on experience!
                         </div>
                         <div class="w-full text-[1.25rem] pr-[5rem] text-c6">
@@ -41,12 +41,11 @@
                         </div>
                     </div>
                 </div>
-                <img src="/src/assets/images/ElsReycel.svg" alt="Group Photo" class="w-[30rem] md:w-1/2 h-auto">
+                <img src="/src/assets/images/ElsReycel.svg" alt="Group Photo" class="w-full md:w-1/2 h-auto">
             </div>
 
             <img src="/src/assets/images/ElsReycel.svg" alt="Photo">
             <img src="/src/assets/images/ElsReycel.svg" alt="Photo">
-            <img src="/src/assets/images/ElsReycel.svg" alt="Photo" class="border-2">
         </div>
 
     </div>
