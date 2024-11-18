e2f84e19c74a57c97c11b5fba60ba998a3b2e9dd
usernameiskheejay <galvez.kjm@optimixr.com>
2024-11-18 23:03:00 +0800

    w5yq4

--- src/components/modals/PreviewPostModal.vue
+++ src/components/modals/PreviewPostModal.vue
@@ -2,11 +2,11 @@
     <div class="z-[2] w-screen h-screen fixed top-0 left-0 flex items-center justify-center px-[1rem] overflow-y-auto pt-[12rem] pb-[6rem] sm:pt-0 sm:pb-0 select-text">
         <div class="bg-c1 opacity-80 w-full h-full fixed left-0 top-0"></div>
         
-        <div class="z-[3] drop-shadow max-w-[55rem] w-[55rem] min-h-[38rem] bg-white rounded-[2.4rem] relative p-[1.25rem]">
+        <div class="z-[3] drop-shadow max-w-[55rem] w-[55rem] min-h-[38rem] bg-white rounded-[2.4rem] relative px-[1.25rem] py-[1.5rem]">
             <img src="/src/assets/images/ProjectsBg.svg" alt="projectBg" 
-                class="absolute bottom-[0.943rem] -left-[1.08rem] w-[25rem] h-auto rounded-[2.4rem] rotate-90 select-none">
+                class="absolute bottom-[0.65rem] sm:bottom-[0.943rem] -left-[0.78rem] sm:-left-[1.08rem] w-[17.5rem] sm:w-[25rem] h-auto rounded-[2.4rem] rotate-90 select-none">
             <img src="/src/assets/images/ProjectsBg.svg" alt="projectBg" 
-                class="absolute top-[0.98rem] -right-[1.08rem] w-[25rem] h-auto rounded-[2.4rem] -rotate-[90deg] select-none">
+                class="absolute top-[0.65rem] sm:top-[0.98rem] -right-[0.75rem] sm:-right-[1.08rem] w-[17.5rem] sm:w-[25rem] h-auto rounded-[2.4rem] -rotate-[90deg] select-none">
 
             <XIcon class="w-8 h-8 text-black absolute right-[1.25rem] top-[1.25rem] z-[2] cursor-pointer active:translate-y-[0.1rem]" />
 
@@ -38,13 +38,13 @@
                     <span class="font-semibold uppercase">senior 56k - 80k, junior 25k - 38k</span>
                 </div>
             </div>
-            <div class="flex flex-col w-full pl-[0.75rem] sm:pl-[1.5rem] relative">
+            <div class="flex flex-col w-full xs:pl-[1.5rem] relative">
                 <span class="text-c1 font-bold text-[1.125rem] pt-4 pb-2">Project Description</span>
                 <div class="drop-shadow bg-c4 w-full min-h-[7rem] p-2 text-[0.90rem] text-font rounded-[0.25rem] overflow-auto no-scrollbar">
                     Project Description
                 </div>
             </div>
-            <div class="w-full flex justify-end pt-8 pr-8 ">
+            <div class="w-full flex justify-center sm:justify-end pt-8 sm:pr-8 ">
                 <button class="drop-shadow px-12 py-1 text-[1.125rem] bg-c1 text-white rounded-full active:translate-y-[0.1rem] duration-100">
                     APPLY NOW
                 </button>
