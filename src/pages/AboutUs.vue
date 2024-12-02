b0647a5a988eca46fdb22ad54c7e53ddf64e0f88
usernameiskheejay <galvez.kjm@optimixr.com>
2024-12-02 21:31:54 +0800

    formalized my identity

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -83,7 +83,7 @@
                         <img src="/src/assets/images/kj.png" alt="Person 1" class="h-[18rem] w-auto" />
                     </div>
                     <div class="font-bold text-white tracking-wider mt-2">
-                        Chicken Bilog
+                        Khee Jay Galvez
                     </div>
                     <div class="text-white tracking-wider">
                         DEVELOPER
